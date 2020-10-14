abd3b3608083e660bcdc4610777878df590df22b
Benjamin Lupton <b@lupton.cc>
2020-10-15 04:03:16 +1100

    add restream

--- source/index.ts
+++ source/index.ts
@@ -48,15 +48,23 @@ const links: Links = {
 	'https://balupton.com/recommends': 'https://balupton.com/recommendations',
 	'https://balupton.com/recommend': 'https://balupton.com/recommendations',
 	'https://balupton.com/gear': 'https://balupton.com/recommendations',
-	// recommendations
+	// actual
 	'https://balupton.com/melon': {
 		name: 'Melon',
 		url: 'https://melonapp.com?ref=balupton',
-		description: 'Use Melon as a free alternative to Zoom and Google Meet that supports streaming',
+		description: 'Use Melon as a free alternative to Zoom, Google Meet, and OBS that supports free streaming to multiple locations',
 		color: '#ff517b',
-		tags: ['referral', 'recommendation'],
+		tags: ['referral'],
 	},
 	'https://bevry.me/r/melon': 'https://balupton.com/melon',
+	'https://balupton.com/restream': {
+		name: 'Restream',
+		url: 'https://restream.io/join/4xozn',
+		description: 'Use Restream as a free alternative to Zoom, Google Meet, and OBS that supports free streaming to multiple locations',
+		color: '#000',
+		tags: ['referral'],
+	},
+	'https://bevry.me/r/restream': 'https://balupton.com/restream',
 	'https://balupton.com/honeygain': {
 		name: 'Honeygain',
 		url: 'https://r.honeygain.money/B57497FAA2',
